3a794fae0f3c8288fb5c9422b7ae0ed2171f79bb
Anurag30112003 <anuragsharma011011@gmail.com>
2023-01-19 02:32:51 +0530

    integrating db

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -31,14 +31,12 @@ async function refreshAccessToken(token){
 }
 
 export default NextAuth ( {
-  // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID   ,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET ,
       authorization: LOGIN_URL , 
     }),
-    // ...add more providers here
   ],
   secret: process.env.JWT_SECRET ,
   pages:{
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,30 +1,34 @@
-import {getProviders , signIn} from 'next-auth/react'
+import { getProviders, signIn } from "next-auth/react";
+import { prisma } from "../db/client";
+function login({ providers }: { providers: any }) {
+  return (
+    <>
+      <div>login</div>
 
-function login({providers}:{providers:any}) {
-  return ( 
-  <>
-    <div>login</div>
-
-    {Object.values(providers).map((provider :any) => (
-      <div key={provider.name}>
-        <button className='bg-[#18D860] text-white p-5 rounded-full'
-        onClick={() => signIn(provider.id , {callbackUrl: '/'})}
-        >Login with {provider.name}</button>
-      </div>
-    ))}
-
-  </>
-  )
+      {Object.values(providers).map((provider: any) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default login
+export default login;
 
-export async function getServerSideProps(){
-    const providers = await getProviders();
-
-    return {
-        props:{
-            providers
-        }
-    }
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  const exists : any = await prisma.user.findMany({
+    where:{email : providers?.email?.id},
+  });
+  return {
+    props: {
+      providers,
+    },
+  };
 }
