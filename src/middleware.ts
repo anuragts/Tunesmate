30980201125c65048224ea8d4fbb2366ab423c44
Anurag30112003 <anuragsharma011011@gmail.com>
2023-01-19 00:46:48 +0530

    middleware working

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+// import { NextApiRequest } from "next";
+import { getToken } from "next-auth/jwt";
+import { NextResponse, NextRequest } from "next/server";
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  const { pathname } = req.nextUrl;
+  if (pathname.includes("/api/auth") || token) {
+    return NextResponse.next();
+  }
+
+  if (!token && pathname !== "/login") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+}
+export const config = {
+  matcher: "/",
+};
