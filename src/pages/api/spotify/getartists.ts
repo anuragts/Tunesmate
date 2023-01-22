dc27e62a550fb6848904a254eb109dd2e910d5e3
Anurag30112003 <anuragsharma011011@gmail.com>
2023-01-22 14:57:35 +0530

    prod break

---
+++ src/pages/api/spotify/getartists.ts
@@ -0,0 +1,4 @@
+import { NextApiRequest , NextApiResponse} from "next"
+export default async ( req:NextApiRequest , res:NextApiResponse) => {
+    res.status(200).json({message : 'working on it..'})
+}
